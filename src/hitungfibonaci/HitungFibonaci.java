b31c94ac411be2723bc5114751296003258e8121
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-12 13:05:29 +0700

    Initial Commit

---
+++ src/hitungfibonaci/HitungFibonaci.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitungfibonaci;
+import java.math.BigInteger;
+import java.util.Scanner;
+public class HitungFibonaci { 
+     public static void main(String[] args)
+    {
+      String identitas = "Devano Abe/XRPL 3/12"; 
+      tampilJudul(identitas);
+
+    } 
+    
+    public static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas " + identitas);    
+        System.out.println("\nHitung Fibonaci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ....dst.\n");
+     
+    }
+   
+     
+}
