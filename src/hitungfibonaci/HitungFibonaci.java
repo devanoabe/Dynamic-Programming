a9de6cae03c22ebd1f3c70671455acebd76d95c9
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-12 13:08:25 +0700

    Tambah Hitung Fibonacci

--- src/hitungfibonaci/HitungFibonaci.java
+++ src/hitungfibonaci/HitungFibonaci.java
@@ -13,6 +13,8 @@ public class HitungFibonaci {
       tampilJudul(identitas);
       
       int n = tampilInput();
+      
+      BigInteger hasil = fibo(n);
 
     } 
     
@@ -34,6 +36,20 @@ public class HitungFibonaci {
           return n;
           
       }
+    
+    private static BigInteger fibo(int n)
+      {
+          BigInteger[] hasil = new BigInteger[n];
+          
+          hasil[0] = BigInteger.ONE;
+          hasil[1] = BigInteger.ONE;
+          
+          for(int i = 2; i < n; i++)
+          {
+              hasil[i] = hasil[i - 1].add(hasil[i - 2]);
+          }
+          return hasil[n - 1];
+      }  
    
      
 }
