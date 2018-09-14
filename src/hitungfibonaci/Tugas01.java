b1c8721364e26be8950de6c4efa024e4107e4f84
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-14 20:51:30 +0700

    Tambah Input Tugas 1

--- src/hitungfibonaci/Tugas01.java
+++ src/hitungfibonaci/Tugas01.java
@@ -15,5 +15,21 @@ public class Tugas01 {
     {
         String identitas = "Devano Abe / X RPL 03 / 12";
         tampilJudul(identitas);
+        
+          System.out.println("Pecahan Uang antara lain Rp.5000, Rp.2000, Rp.1000, Rp.500, Rp.100 ");
+        int uang = input();
+        int[] Nominal = new int[]{5000, 2000, 1000, 500, 100};
+       
+    }
+    
+    private static int input()
+    {
+        Scanner baca = new Scanner (System.in);
+        
+        System.out.print("Jumlah uang = Rp. ");
+        int uang = baca.nextInt();
+        
+        return uang;
+    
     }
 }
