a0b8c02e926c902022f5383913b048d6181bd63d
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-14 20:49:40 +0700

    Identitas Tugas 1

---
+++ src/hitungfibonaci/Tugas01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitungfibonaci;
+import java.util.Scanner;
+public class Tugas01 {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas :" + identitas);
+    }
+    
+    public static void main(String[] args)
+    {
+        String identitas = "Devano Abe / X RPL 03 / 12";
+        tampilJudul(identitas);
+    }
+}
